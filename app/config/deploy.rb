caa0e8b6a595befa93fe0a7b405ebf90ad7d6c6c
Vladimir Odesskij <odesskij1992@gmail.com>
2015-05-30 00:33:15 +0300

    tests

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,15 +1,15 @@
 default_run_options[:pty] = true
 
-set :use_sudo,    false
-set :application, "capifony"
-set :user,        "capifony"
-set :domain,      "capifony.alpha.branderstudio.com"
-set :deploy_to,   "/home/capifony/www"
-set :app_path,    "app"
-set :repository,  "https://github.com/odesskij/blog.git"
-set :scm,         :git
-
-set :model_manager, "doctrine"
+set :use_sudo,          false
+set :application,       "capifony"
+set :user,              "capifony"
+set :domain,            "capifony.alpha.branderstudio.com"
+set :deploy_to,         "/home/capifony/www"
+set :app_path,          "app"
+set :repository,        "https://github.com/odesskij/blog.git"
+set :scm,               :git
+set :shared_files,      ["app/config/parameters.yml"]
+set :model_manager,     "doctrine"
 
 role :web,        domain
 role :app,        domain, :primary => true
@@ -17,4 +17,4 @@ role :app,        domain, :primary => true
 set  :keep_releases,  3
 
 # Be more verbose by uncommenting the following line
-logger.level = Logger::MAX_LEVEL
+# logger.level = Logger::MAX_LEVEL
